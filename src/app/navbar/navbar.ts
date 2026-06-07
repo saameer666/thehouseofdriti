db84368af97a07602ba3fb97642261f280c9cfb5
veera3264-cmyk <veera3264@gmail.com>
2026-06-07 15:39:51 -0400

    Initial commit

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {}
